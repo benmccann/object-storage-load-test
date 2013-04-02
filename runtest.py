796d4f3cb3a01175f2bea95b16019138c3f2f4ee
Ben McCann <ben@benmccann.com>
2013-04-01 19:24:02 -0700

    Another try at actually making this multi-threaded

--- runtest.py
+++ runtest.py
@@ -15,17 +15,21 @@ parser.add_argument('--password', type=str, dest='password', required=True,
 args = parser.parse_args()
 
 def upload(data):
-  sl_storage = object_storage.get_client(args.username, args.password, datacenter='dal05')
   remote_filename = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(16)) + '.html'
-  sl_storage['loadtest'][remote_filename].create()
-  sl_storage['loadtest'][remote_filename].send(data)
+  try:
+    # print 'Uploading ' + remote_filename
+    sl_storage = object_storage.get_client(args.username, args.password, datacenter='dal05')
+    sl_storage['loadtest'][remote_filename].create()
+    sl_storage['loadtest'][remote_filename].send(data)
+  except object_storage.errors.ResponseError, e:
+    print 'Received ' + e.status + ': ' + e.reason
   time.sleep(1)
   upload(data)
 
 with open ('test.html', 'r') as myfile:
   data = myfile.read()
-  pool = Pool(processes=100)
-  for i in range(100):
+  pool = Pool(processes=10)
+  for i in range(20):
     pool.apply_async(upload, args=(data,))
   pool.close()
   pool.join()
